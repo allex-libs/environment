a6310e18648a039f3d9e5322fdab792bdd5b2998
Veljko Popovic <v.s.popovic@gmail.com>
2016-07-13 14:31:43 +0200

    introduced browserify and web_component

---
+++ browserify.js
@@ -0,0 +1,2 @@
+ALLEX.execSuite.libRegistry.register('allex_environmentlib',require('./src/index')(ALLEX));
+ALLEX.WEB_COMPONENTS.allex_environmentlib = ALLEX.execSuite.libRegistry.get('allex_environmentlib');
