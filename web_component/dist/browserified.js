4446cb2b4eecd94de78efccfe524bece2882f50e
Andrija Petrovic <andrija.hers@gmail.com>
2017-02-10 12:07:16 +0100

    `hook` is out, `query` is in

--- web_component/dist/browserified.js
+++ web_component/dist/browserified.js
@@ -1378,7 +1378,7 @@ function createAllexHash2ArrayDataSource (execlib, AllexState) {
 module.exports = createAllexHash2ArrayDataSource;
 
 },{}],10:[function(require,module,exports){
-function createAllexLevelDBDataSource(execlib, DataSourceSinkBase, BusyLogic, LevelDBProxy) {
+function createAllexLevelDBDataSource(execlib, DataSourceSinkBase, BusyLogic) {
   'use strict';
 
   var lib = execlib.lib,
@@ -1386,14 +1386,12 @@ function createAllexLevelDBDataSource(execlib, DataSourceSinkBase, BusyLogic, Le
     q = lib.q,
     VALID_HOOK_TYPES = {
       'data' : {
-        channel : 'l', 
-        init : {},
-        command : 'hook'
+        init : lib.Map,
+        command : 'query'
       },
       'log' : {
-        channel : 'g',
         init : [],
-        command : 'hookToLog'
+        command : 'queryLog'
       }
     };
 
@@ -1402,16 +1400,26 @@ function createAllexLevelDBDataSource(execlib, DataSourceSinkBase, BusyLogic, Le
   }
   function AllexLevelDB (sink, options) {
     DataSourceSinkBase.call(this,sink, options); //nisam bas najsigurniji ...
+    var init;
     this._sink_name = options.sink;
+    this._filter = options.filter || {};
     this._bl = new BusyLogic(this);
     this.hook_params = options.hook_params ? options.hook_params : {scan : true, accounts : ['***']};
     this.hook_type = options.hook_type ? options.hook_type : 'data';
     if (!(this.hook_type in VALID_HOOK_TYPES)) throw new Error ('Invalid hook type : '+options.hook_type);
-    this.data = lib.extend (VALID_HOOK_TYPES[this.hook_type].init);
+    this.data = null;
+    init = VALID_HOOK_TYPES[this.hook_type].init;
+    if (lib.isFunction(init)) {
+      this.data = new init;
+    }
+    if (lib.isArray(init)) {
+      this.data = init.slice();
+    }
   }
   lib.inherit(AllexLevelDB, DataSourceSinkBase);
   AllexLevelDB.prototype.destroy = function () {
     this._sink_name = null;
+    this._filter = null;
     this.hook_params = null;
     this._bl.destroy();
     this._bl = null;
@@ -1420,36 +1428,45 @@ function createAllexLevelDBDataSource(execlib, DataSourceSinkBase, BusyLogic, Le
   };
 
   AllexLevelDB.prototype._doGoWithSink = function (sink) {
-    LevelDBProxy.consumeChannel(this._sink_name, sink, VALID_HOOK_TYPES[this.hook_type].channel, this.onLevelDBData.bind(this));
-    sink.sessionCall(VALID_HOOK_TYPES[this.hook_type].command, this.hook_params);
+    //sink.sessionCall(VALID_HOOK_TYPES[this.hook_type].command, this.hook_params);
+    taskRegistry.run('queryLevelDB', {
+      sink: sink,
+      queryMethodName: VALID_HOOK_TYPES[this.hook_type].command,
+      filter: this._filter,
+      scanInitially: true,
+      onPut: this.onLevelDBData.bind(this),
+      onDel: this.onLevelDBData.bind(this),
+      onInit: console.log.bind(console, 'queryLevelDB init')
+    });
     return q.resolve(true);
   };
 
   function fromarrayToData (key, data, val) {
     if (key.length === 1) {
-      data[key[0]] = val;
+      data.replace(key[0], val);
       return;
     }
 
     var k = key.shift();
-    if (!data.hasOwnProperty(k)) {
-      data[k] = {};
+    if (!lib.isVal(data.get(k))) {
+      data.add(k, new lib.Map());
     }
-    fromarrayToData (key, data[k], val);
+    fromarrayToData (key, data.get(k), val);
   }
 
 
-  AllexLevelDB.prototype._processHook = function (leveldata) {
+  AllexLevelDB.prototype._processMap = function (leveldata) {
     var key = leveldata[0];
     if (lib.isArray(key)){
       fromarrayToData (key.slice(), this.data, leveldata[1]);
     }else{
-      this.data[leveldata[0]] = leveldata[1];
+      //this.data[leveldata[0]] = leveldata[1];
+      this.data.replace(leveldata[0], leveldata[1]);
     }
     this._bl.emitData();
   };
 
-  AllexLevelDB.prototype._processHookToLog = function (leveldata) {
+  AllexLevelDB.prototype._processArray = function (leveldata) {
     this.data.push (leveldata);
     this._bl.emitData();
   };
@@ -1459,12 +1476,12 @@ function createAllexLevelDBDataSource(execlib, DataSourceSinkBase, BusyLogic, Le
     if (!leveldata) return;
 
     if (this.hook_type === 'data') {
-      this._processHook(leveldata);
+      this._processMap(leveldata);
       return;
     }
 
     if (this.hook_type === 'log') {
-      this._processHookToLog (leveldata);
+      this._processArray (leveldata);
       return;
     }
   };
@@ -1477,7 +1494,8 @@ function createAllexLevelDBDataSource(execlib, DataSourceSinkBase, BusyLogic, Le
   AllexLevelDB.prototype.copyData = function () {
     switch (this.hook_type) {
       case 'log' : return this.data.slice();
-      case 'data': return lib.extend({}, this.data);
+      //case 'data': return lib.extend({}, this.data);
+      case 'data': return this.data;
     }
     throw new Error('Unknow hook type', this.hook_type);
   };
